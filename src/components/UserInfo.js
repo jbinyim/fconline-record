ff72166951f2bbe7dc75eb0a993e178ded657eb3
SONGDONGGEON <songdonggeon0805@gmail.com>
2024-04-21 18:54:23 +0900

    유저인포 제작중

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,7 +2,28 @@ import React from "react";
 
 const UserInfo = ({ userOuid }) => {
   console.log(userOuid);
-  return <div></div>;
+  return <div>
+    <section class="infoArea">
+        <div>
+          <div>
+            <img src="https://ssl.nexon.com/s2/game/fo4/obt/rank/large/update_2009/ico_rank1.png"></img>
+            <p>공식경기 1:1 역대 최고 티어 - 챔피언스</p>
+            <p>최고 티어 달성 날짜 - 0000.00.00</p>
+          </div>
+          <div>
+            <img src="https://ssl.nexon.com/s2/game/fo4/obt/rank/large/update_2009/ico_rank1.png"></img>
+            <p>볼타 역대 최고 티어 - 루키</p>
+            <p>최고 티어 달성 날짜 - 0000.00.00</p>
+          </div>
+          <div>
+              <h2>닉네임 : 발긔</h2>
+              <p>LV.100</p>
+          </div>
+        </div>
+        <button type="button">전적갱신</button>
+    </section>
+    
+  </div>;
 };
 
 export default UserInfo;
