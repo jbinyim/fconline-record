1048ac0a2cfcaf47d0d45a932a992e36dfbe3dad
SONGDONGGEON <songdonggeon0805@gmail.com>
2024-05-02 02:03:59 +0900

    헤더 세부작업중

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,37 @@ import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
+
+  const enter = (e) => {
+    if (e.key === "Enter") {
+      const ouid = e.target.value;
+      if (!ouid) {
+        alert("구단주명을 입력해주세요.");
+      } else {
+        navigate(`/userinfo/?q=${ouid}`);
+        e.target.value = "";
+      }
+    }
+  };
   return (
     <div>
       <header>
-            <h1 class="outline-button" onClick={() => navigate("/")}>FC GG.KR</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="https://fconline.nexon.com/main/index" target="_blank">
-                FC Online
-              </a>
-            </li>
-            <li><a>선수 검색</a></li>
-            <li><a>랭커 닉네임조회</a></li>
-          </ul>
-        </nav>
+            <h1 onClick={() => navigate("/")}>FC GG.KR</h1>
+            <nav>
+              <ul>
+                <li>
+                  <a href="https://fconline.nexon.com/main/index" target="_blank">
+                    FC Online
+                  </a>
+                </li>
+                <li><a class="hovercolor">선수 검색</a></li>
+                <li>
+                  <input type="checkbox" id="toggleBtn"></input>
+                  <label for="toggleBtn" className="toggleBtn">닉네임조회</label>
+                  <input onKeyUp={enter} type="text" className="textbox"/>
+                </li>
+              </ul>
+            </nav>
       </header>
     </div>
   );
