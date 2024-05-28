17d184b91c5e945e9c4d5d30b9b46509301a9bfb
SONGDONGGEON <songdonggeon0805@gmail.com>
2024-05-28 23:12:28 +0900

    섹스

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,21 +39,91 @@ const Header = () => {
 
   return (
     <div>
-      <div>
-        <h1 onClick={goToHome}>FC.GG</h1>
-        <ul>
-          <li>선수검색</li>
-          <li>fconline</li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li>공지사항</li>
-        </ul>
-      </div>
-      <div>
-        <input onKeyUp={onSearch} type="text" />
-      </div>
+        <header>
+              <h1 onClick={goToHome}>FC GG.KR</h1>
+              <nav>
+                <ul>
+                  <li>
+                    <a href="https://fconline.nexon.com/main/index" target="_blank" className="hovercolor">
+                      FC Online
+                    </a>
+                  </li>
+                  <li>
+                    <a className="hovercolor">선수 검색</a>
+                  </li>
+                </ul>
+              </nav>
+            </header>
+
+            <section className="header2">
+              <div>
+                <ul>
+                  <li>
+                    공지사항
+                    <div className="subMenu">
+                      <ul>
+                        <li><a href="https://fconline.nexon.com/news/notice/list" target="_blank">공지사항</a></li>
+                        <li><a href="https://fconline.nexon.com/news/play/list" target="_blank">개발자 노트</a></li>
+                        <li><a href="https://fconline.nexon.com/news/nf/list" target="_blank">넥스트 필드</a></li>
+                        <li><a href="https://fconline.nexon.com/news/update/list" target="_blank">업데이트</a></li>
+                        <li><a href="https://fconline.nexon.com/news/events/list" target="_blank">이벤트</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    커뮤니티
+                    <div className="subMenu">
+                      <ul>
+                        <li><a href="https://fconline.nexon.com/community/recommend" target="_blank">추천 게시판</a></li>
+                        <li><a href="https://fconline.nexon.com/community/free/list" target="_blank">자유 게시판</a></li>
+                        <li><a href="https://fconline.nexon.com/community/tip/list" target="_blank">팁</a></li>
+                        <li><a href="https://fconline.nexon.com/community/qa/list" target="_blank">질문 게시판</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    클럽
+                    <div className="subMenu">
+                      <ul>
+                        <li><a href="https://fconline.nexon.com/Club/List" target="_blank">내 클럽</a></li>
+                        <li><a href="https://fconline.nexon.com/Club/List" target="_blank">클럽 리스트</a></li>
+                        <li><a href="https://fconline.nexon.com/ClubPromotion/PromotionList" target="_blank">클럽 홍보</a></li>
+                        <li><a href="https://fconline.nexon.com/Club/Create" target="_blank">클럽 생성</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    웹 상점
+                    <div className="subMenu">
+                      <ul>
+                        <li><a href="https://shop.fconline.nexon.com/" target="_blank">웹 상점</a></li>
+                        <li><a href="https://shop.fconline.nexon.com/Membership" target="_blank">멤버십</a></li>
+                        <li><a href="https://shop.fconline.nexon.com/ShopEvent/Index" target="_blank">스페셜 상점</a></li>
+                        <li><a href="https://shop.fconline.nexon.com/mypage" target="_blank">마이페이지</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    랭킹
+                    <div className="subMenu">
+                      <ul>
+                        <li><a href="https://fconline.nexon.com/datacenter/rank" target="_blank">공식경기 랭킹</a></li>
+                        <li><a href="https://fconline.nexon.com/datacenter/rank_volta_topplayer" target="_blank">볼타라이브 랭킹</a></li>
+                        <li><a href="https://fconline.nexon.com/datacenter/rank_arena" target="_blank">아레나 모드 랭킹</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li><a href="https://fconline.nexon.com/datacenter/squadmaker" target="_blank">스쿼드메이커</a></li>
+                  <li><a href="https://fconline.nexon.com/datacenter/playergrow" target="_blank">강화부스트 도우미</a></li>
+                </ul>
+              </div>
+            </section>
+        <section className="searchArea">
+          <div>
+            <input onKeyUp={onSearch} type="text" />
+            <span>검색아이콘</span>
+          </div>
+        </section>
     </div>
   );
 };
