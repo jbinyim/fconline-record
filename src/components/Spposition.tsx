689515e08b80478b3c9d85e71084041ed3402a06
jbinyim <jbinyim12@naver.com>
2024-06-04 01:03:00 +0900

    240604 ...

--- src/components/Spposition.tsx
+++ src/components/Spposition.tsx
@@ -15,7 +15,7 @@ const Spposition = ({ item }: ISpposition) => {
   const players01 = item.matchInfo[0].player.map((data: IPlater) => data);
   console.log(players01);
   return (
-    <div>
+    <div style={{ height: "100px" }}>
       <div>
         {players01.map((player) => (
           <div key={player.spId}>
